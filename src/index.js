b38e926ffc181399f0ec5ffd455f64f10f1a2502
Victoria Le <leqvictoria@gmail.com>
2022-10-28 16:09:33 -0400

    default values for body tag

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
     ,
   </React.StrictMode>,
   document.getElementById('root')
