3b9dd9505693ab90d9ed3ba12b1b4e5dcb332ab8
Victoria Le <leqvictoria@gmail.com>
2022-11-01 12:41:40 -0400

    nav links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss'
 import LogoV from '../../assets/images/logo-v.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
     return (
@@ -12,10 +12,19 @@ const Sidebar = () => {
                 <img src={LogoV} alt='logo' />
                 <img  className='sub-logo' src={LogoSubtitle} alt='victoria' />
             </Link>
+            {/* END OF LOGO AND SUBTITLE */}
 
             <nav>
                 <NavLink exact='true' activeclassname='active' to='/'>
                     <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+                </NavLink>
+
+                <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
+                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+                </NavLink>
+
+                <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact'>
+                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
                 </NavLink>
             </nav>
         </div>
