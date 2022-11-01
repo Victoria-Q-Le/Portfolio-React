bc3c317913f452406bd3629b0bf4ac54d3e6d3ec
Victoria Le <leqvictoria@gmail.com>
2022-11-01 17:32:08 -0400

    setting home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    Hi,
+                    <br />
+                    I'm 
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,25 @@
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
-    return <>Hello from Layout with the <Sidebar /> </>
+    return (
+        <div className='App'>
+            <Sidebar /> 
+            {/* The main wrapper of the application */}
+            <div className='page'> 
+                <span className='tags top-tags'>&lt;body&gt;</span>
+
+                <Outlet />
+
+                <span className='tags bottom-tags'>
+                    &lt;/body&gt;
+                    <br />
+                    <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                </span>
+            </div>
+        </div>
+    )
 }
 
 export default Layout
