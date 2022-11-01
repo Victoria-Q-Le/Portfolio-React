8a8ca656daaff098a28ed65ec62dddda93e791f9
Victoria Le <leqvictoria@gmail.com>
2022-11-01 18:26:44 -0400

    container styling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,7 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-v.png'
+import './index.scss'
+
 const Home = () => {
     return (
         <div className="container home-page">
@@ -6,7 +10,13 @@ const Home = () => {
                     Hi,
                     <br />
                     I'm 
+                    <img src={LogoTitle} alt='developer'/>
+                    ictoria
+                    <br />
+                    web developer
                 </h1>
+                <h2>Full Stack Developer</h2>
+                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
     )
