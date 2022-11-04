aa97ab372bb3d8bc12680c220b788b5645b6d6e9
Victoria Le <leqvictoria@gmail.com>
2022-11-03 20:37:44 -0400

    fixed curly errors

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,8 +16,8 @@ const Home = () => {
                     <span className={letterClass}>H</span>
                     <span className={`${letterClass} _12`}>i,</span>
                     <br />
-                    <span className={'${letterClass} _13'}>I</span>
-                    <span className={'${letterClass} _14'}>'m</span>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
                     <img src={LogoTitle} alt='developer'/>
                     <AnimatedLetters  letterClass={letterClass} strArray={nameArray} idx={15}/>
                     <br />
