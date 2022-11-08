0c0e0a1b48c54e7c0669bc1e4ce4d86e238fedc5
Victoria Le <leqvictoria@gmail.com>
2022-11-07 21:22:19 -0500

    linked animated letters to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,3 +1,20 @@
-const About = () => ('Hello')
+import './index.scss' 
+import AnimatedLetters from '../AnimatedLetters'
+
+const About = () => {
+    
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters 
+                        strArray={['A','b','o','u','t',' ','m','e']}
+                        idx={15}
+                    />
+                </h1>
+            </div>
+        </div>
+    )
+}
 
 export default About
