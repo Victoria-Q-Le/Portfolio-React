358f347bb8e8da5c37c92ad6ddcf3e789c7a5ec6
Victoria Le <leqvictoria@gmail.com>
2022-11-08 11:27:39 -0500

    styling about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,6 +12,16 @@ const About = () => {
                         idx={15}
                     />
                 </h1>
+                <p>
+                    I'm an ambitious software engineer looking for a role in tech industry with the opportunity to work with the latest technologies on challenging and diverse projects 
+                </p>
+                <p>
+                    I'm a curious life long learner and perpetually working on improving my stacks one project at a time
+                </p>
+                <p>
+                    If I need to define myself in one sentence that would be a family person, travelling enthusiast and tech-obsessed!
+                </p>
+
             </div>
         </div>
     )
