c0875c34b83e042918705dd12ee5bc6a0b0807c8
Victoria Le <leqvictoria@gmail.com>
2022-11-08 12:31:33 -0500

    animation added to about page title

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,23 @@
 import './index.scss' 
 import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        },4000)
+    }, [])
     
     return (
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
-                    <AnimatedLetters 
+                    <AnimatedLetters
+                        letterClass={letterClass} 
                         strArray={['A','b','o','u','t',' ','m','e']}
                         idx={15}
                     />
