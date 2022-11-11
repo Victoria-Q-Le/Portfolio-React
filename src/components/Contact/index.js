a4c8bdfc3310ebdce9c2b5d7e804e377153bcfb9
Victoria Le <leqvictoria@gmail.com>
2022-11-11 09:41:56 -0800

    contact page linked and loader added

---
+++ src/components/Contact/index.js
@@ -0,0 +1,16 @@
+import Loader from 'react-loaders'
+import './index.scss'
+
+
+const Contact = () => {
+    
+    return (
+        <>
+            <div className='container contact-page'>
+                <Loader type='pacman' />
+            </div>
+        </>
+    )
+}
+
+export default Contact 
