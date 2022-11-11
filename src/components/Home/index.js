f41b87e4570f0a956493c7ed92ff083639d55f45
Victoria Le <leqvictoria@gmail.com>
2022-11-11 09:32:55 -0800

    loader added to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import LogoTitle from '../../assets/images/logo-v.png'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -19,25 +20,30 @@ const Home = () => {
     // The useEffect to ensure the effect only runs once, after loading the letterClass will be changed to text-animate-hover
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                    <img src={LogoTitle} alt='developer'/>
-                    <AnimatedLetters  letterClass={letterClass} strArray={nameArray} idx={15}/>
-                    <br />
-                    <AnimatedLetters  letterClass={letterClass} strArray={jobArray} idx={22}/>
-                </h1>
-                <h2>Software Engineer | Full Stack Developer </h2>
-                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
-            </div>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <img src={LogoTitle} alt='developer'/>
+                        <AnimatedLetters  letterClass={letterClass} strArray={nameArray} idx={15}/>
+                        <br />
+                        <AnimatedLetters  letterClass={letterClass} strArray={jobArray} idx={22}/>
+                    </h1>
+                    <h2>Software Engineer | Full Stack Developer </h2>
+                    <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+                </div>
 
-            <Logo />
-        </div>
+                <Logo />
+            </div>
+            {/* THE END OF HOME PAGE CONTAINER */}
+
+            <Loader type='pacman' />
+        </>
     )
 }
 
