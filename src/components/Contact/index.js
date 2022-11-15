a9ecbfd845623f9c53b031c3c137b6623f0f6733
Victoria Le <leqvictoria@gmail.com>
2022-11-15 15:20:52 -0500

    contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,14 +1,20 @@
 import Loader from 'react-loaders'
 import './index.scss'
-
+import AnimatedLetters from '../AnimatedLetters'
 
 const Contact = () => {
     
     return (
         <>
             <div className='container contact-page'>
-                <Loader type='pacman' />
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters  strArray={['C','o','n','t','a','c','t',' ','m','e']}/>
+                    </h1>
+                </div>
             </div>
+
+            <Loader type='pacman' />
         </>
     )
 }
