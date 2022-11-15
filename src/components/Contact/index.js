49a0fb316aa8f3e34ce0be046f844e80d26f3905
Victoria Le <leqvictoria@gmail.com>
2022-11-15 15:28:13 -0500

    contact page animation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,27 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        },3000)
+    },[])
     
     return (
         <>
             <div className='container contact-page'>
                 <div className='text-zone'>
                     <h1>
-                        <AnimatedLetters  strArray={['C','o','n','t','a','c','t',' ','m','e']}/>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['C','o','n','t','a','c','t',' ','m','e']}
+                            idx={15}
+                        />
                     </h1>
                 </div>
             </div>
