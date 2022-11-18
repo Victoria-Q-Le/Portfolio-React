ceaaf85de987c37cc0c531e80476f10b88be8224
Victoria Le <leqvictoria@gmail.com>
2022-11-18 14:47:33 -0500

    adding loader to portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,8 +1,18 @@
+import Loader from 'react-loaders'
 import './index.scss'
 
 const Portfolio = () => {
     return (
-        <div>Hello from Portfolio</div>
+        <>
+            <div className='container portfolio-page'>
+                <h1 className='page-title'>
+                    Portfolio
+                </h1>
+
+                <Loader type='pacman' />
+            </div>
+        </>
+        
     )
 }
 
