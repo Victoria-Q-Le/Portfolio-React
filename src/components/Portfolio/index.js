7ce5cacc09c52fa1e79798c13f7ee19cd74d3638
Victoria Le <leqvictoria@gmail.com>
2022-11-18 17:36:29 -0500

    refactor layout scss

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Portfolio = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    
+    useEffect(() => {
+        setTimeout(() =>{
+            setLetterClass('text-animate-hover')
+        },3000)
+    },[])
     return (
         <>
             <div className='container portfolio-page'>
                 <h1 className='page-title'>
-                    Portfolio
+                    <AnimatedLetters 
+                        letterClass={letterClass} 
+                        strArray={"Portfolio".split("")} //instead of entering letter by letter, split fuction was used to split and return the letters in an array
+                        idx={15}/>
                 </h1>
 
                 <Loader type='pacman' />
