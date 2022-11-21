b94a1a59ac506644bf64dbc57117f4204803a980
Victoria Le <leqvictoria@gmail.com>
2022-11-20 22:43:51 -0500

    styling adjustment

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -17,24 +17,30 @@ const Portfolio = () => {
 
     const renderPortfolio = (portfolio) => {
         return (
-            <div className='images-container'>
+            <div className="images-container">
                 {
                     portfolio.map((port, idx) => {
-                        return(
+                        return (
                             <div className="image-box" key={idx}>
                                 <img 
-                                    className='portfolio-image'
-                                    src={port.cover} 
-                                    alt="portfolio"
-                                />
+                                src={port.cover}
+                                className="portfolio-image"
+                                alt="portfolio" />
+                                <div className="content">
+                                    <p className="title">{port.name}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
                             </div>
                         )
                     })
                 }
             </div>
-        )
+        );
     }
-
 
     return (
         <>
