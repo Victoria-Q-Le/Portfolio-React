26be591cddf3cd2ca968c342d8ef22f1f4601fed
Victoria Le <leqvictoria@gmail.com>
2022-11-29 21:38:30 -0500

    google auth

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,9 @@
 const Login = () => {
     return (
-        <div>
+        <div className="dashboard">
+            <button>
+                Sign in with Google
+            </button>
 
         </div>
     )
