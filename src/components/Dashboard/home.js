07457ccf431ed700f1d7ab13499e1bd0d9eddb5a
Victoria Le <leqvictoria@gmail.com>
2022-11-30 15:01:33 -0500

    form for adding project

--- src/components/Dashboard/home.js
+++ src/components/Dashboard/home.js
@@ -1,7 +1,20 @@
 const Home = () => {
     return (
-        <div>
-            Hello from Home in dashboard
+        <div className="dashboard">
+           <form>
+                <p>
+                    <input type='text' placeholder = "Name" />
+                </p>
+                <p>
+                    <textarea placeholder = "Description" />
+                </p>
+                <p>
+                    <input type='text' placeholder = "Link to Site" />
+                </p>
+                <p>
+                    <input type='file' placeholder = "Cover" />
+                </p>
+           </form>
         </div>
     )
 }
