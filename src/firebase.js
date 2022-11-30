fa9d5aef2c3abd0ce636982f6fb785139a99e0c2
Victoria Le <leqvictoria@gmail.com>
2022-11-29 21:22:44 -0500

    Hooking up to firebase and Login comp created

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2DMwkYerBHiTJVzvcZ8tUI_RIVFCy-zM",
+  authDomain: "react-portfolio-dashboar-4c88e.firebaseapp.com",
+  projectId: "react-portfolio-dashboar-4c88e",
+  storageBucket: "react-portfolio-dashboar-4c88e.appspot.com",
+  messagingSenderId: "939640507262",
+  appId: "1:939640507262:web:605aba893a0b17ff02812f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
