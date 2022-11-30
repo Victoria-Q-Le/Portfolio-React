a07ff7dc39d960cb07e43d64d6fc7ab1e61a21ba
Victoria Le <leqvictoria@gmail.com>
2022-11-30 11:15:44 -0500

    linked Dashboard to app, start working auth state

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,4 +1,15 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { useEffect, useState } from "react"
+
 const Dashboard = () => {
+
+    const [user, setUser] = useState(null)
+    const auth = getAuth()
+
+    useEffect(() => {
+        onAuthStateChanged()
+    }, [])
+    
     return (
         <div> 
             Hello from the Dashboard
@@ -7,3 +18,4 @@ const Dashboard = () => {
 }
 
 export default Dashboard
+
