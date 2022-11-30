1fb8c188e69394d376e7e1b819da1cf83c6f085a
Victoria Le <leqvictoria@gmail.com>
2022-11-30 11:22:54 -0500

    finished firebase authentication

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,7 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { useEffect, useState } from "react"
+import Login from "../Login"
+import Home from "./home"
 
 const Dashboard = () => {
 
@@ -7,12 +9,18 @@ const Dashboard = () => {
     const auth = getAuth()
 
     useEffect(() => {
-        onAuthStateChanged()
+        onAuthStateChanged(auth, (user) => {
+            if(user) { //if user is existed/loginin, the user will become user and display the dashboard 
+                setUser(user)
+            } else {
+                setUser(null)
+            }
+        })
     }, [])
-    
+
     return (
         <div> 
-            Hello from the Dashboard
+            {user ? <Home /> : <Login />}
         </div>
     )
 }
