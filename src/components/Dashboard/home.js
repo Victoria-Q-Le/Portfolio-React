79f9988486e3ff583d84e1f5f80374792645ca3f
Victoria Le <leqvictoria@gmail.com>
2022-12-01 11:58:27 -0500

    submit button

--- src/components/Dashboard/home.js
+++ src/components/Dashboard/home.js
@@ -2,18 +2,11 @@ const Home = () => {
     return (
         <div className="dashboard">
            <form>
-                <p>
-                    <input type='text' placeholder = "Name" />
-                </p>
-                <p>
-                    <textarea placeholder = "Description" />
-                </p>
-                <p>
-                    <input type='text' placeholder = "Link to Site" />
-                </p>
-                <p>
-                    <input type='file' placeholder = "Cover" />
-                </p>
+                <p> <input type='text' placeholder = "Title" /> </p>
+                <p> <textarea placeholder = "Description" /> </p>
+                <p> <input type='text' placeholder = "Link to Site" /> </p>
+                <p> <input type='file' placeholder = "Cover" /> </p>
+                <button>Submit</button>
            </form>
         </div>
     )
