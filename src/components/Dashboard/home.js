db3c378413920018b64ab02fa9aac6422db2ac92
Victoria Le <leqvictoria@gmail.com>
2022-12-06 20:22:09 -0500

    able to retrieve information from fields

--- src/components/Dashboard/home.js
+++ src/components/Dashboard/home.js
@@ -1,9 +1,24 @@
+import { useRef } from "react"
 import { auth } from "../../firebase"
 
 const Home = () => {
+
+    const form = useRef()
+
+    const submitPortfolio =(e) => {
+        e.preventDefault()
+        const title = form.current[0]?.value
+        const description = form.current[1]?.value
+        const url = form.current[2]?.value
+        const cover = form.current[3]?.files[0] //input is not text, so type is file and only 1 file so the idx is 0
+
+        console.log(title, description, url, cover);
+        
+    }
+
     return (
         <div className="dashboard">
-            <form>
+            <form ref={form} onSubmit={submitPortfolio}>
                 <p><input type='text' placeholder="Title"></input></p>
                 <p><textarea placeholder="Description"></textarea></p>
                 <p><input type='text' placeholder="Link to Site"></input></p>
@@ -12,7 +27,7 @@ const Home = () => {
                 <button onClick={() => auth.signOut()}>Sign out</button>
                 
                                 
-             </form>
+            </form>
         </div>
     )
 } 
