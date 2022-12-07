85d63503c313b64c932471b9a27a20c81d25530c
Victoria Le <leqvictoria@gmail.com>
2022-12-06 20:10:45 -0500

    sign out working

--- src/components/Dashboard/home.js
+++ src/components/Dashboard/home.js
@@ -1,31 +1,20 @@
-import { useRef } from 'react'
-import {auth} from '../../firebase'
+import { auth } from "../../firebase"
 
 const Home = () => {
-
-    const form = useRef()
-    
-    const submitPortfolio = (e) => {
-        e.preventDefault()
-        const title = form.current[0]?.value
-        const decription = form.current[1]?.value
-        const url = form.current[2]?.value
-        const cover = form.current[3]?.files[0] //because this is not a text so we will access the file, and we only allowed 1 img for the file idx is 0
-
-    }
-
     return (
         <div className="dashboard">
-           <form ref={form} onSubmit={submitPortfolio()}>
-                <p> <input type='text' placeholder = "Title" /> </p>
-                <p> <textarea placeholder = "Description" /> </p>
-                <p> <input type='text' placeholder = "Link to Site" /> </p>
-                <p> <input type='file' placeholder = "Cover" /> </p>
-                <button type="submit">Submit</button>
-                <button onClick={() => auth.signOut()}>Log Out </button>
-           </form>
+            <form>
+                <p><input type='text' placeholder="Title"></input></p>
+                <p><textarea placeholder="Description"></textarea></p>
+                <p><input type='text' placeholder="Link to Site"></input></p>
+                <p><input type='file' placeholder="Cover Photo"></input></p>
+                <button type='submit'>Submit</button>
+                <button onClick={() => auth.signOut()}>Sign out</button>
+                
+                                
+             </form>
         </div>
     )
-}
+} 
 
 export default Home 
