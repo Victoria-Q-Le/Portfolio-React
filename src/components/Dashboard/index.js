0929be853df21825f014658da8d4d9ab267172a5
Victoria Le <leqvictoria@gmail.com>
2022-12-08 22:00:57 -0500

    watch for auth state changes

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,11 +1,25 @@
 // This component is to check whether the user is logged in, 
 //     if yes, display the dashboard
 //     if no, display the log in page 
-//  The useEffect will be used to watch on the auth state changes, to render appropriate components
+
+import { useEffect, useState } from "react"
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
 const Dashboard = () => {
+
+    const [user,setUser] = useState(null)
+
+    const auth = getAuth() //authenticate the user
+
+    useEffect(() => {
+        onAuthStateChanged() //2 args: the auth state and the method 
+    }, []) // performed only once <=> []
+
     return (
         <div>Hello</div>
     )
 }
 
 export default Dashboard
+
+//  The useEffect will be used to watch on the auth state changes, to render appropriate components. To do this, I used the onAuthStateChanged method = whenever the authenticated state changed diff components will change. 
