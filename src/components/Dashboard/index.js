b48ce172d589d2a5d4102062724ac6b532a985e6
Victoria Le <leqvictoria@gmail.com>
2022-12-08 21:47:30 -0500

    useEffect to watch auth state changes

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,4 +1,11 @@
 // This component is to check whether the user is logged in, 
 //     if yes, display the dashboard
 //     if no, display the log in page 
+//  The useEffect will be used to watch on the auth state changes, to render appropriate components
+const Dashboard = () => {
+    return (
+        <div>Hello</div>
+    )
+}
 
+export default Dashboard
