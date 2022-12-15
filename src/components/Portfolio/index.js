49e7b54164856f067db8408d031fbf7f638fc4d0
Victoria Le <leqvictoria@gmail.com>
2022-12-15 15:27:26 -0500

    adjust the name convention, replace static db by dynamic one

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
-import portfolioData from '../../data/portfolio.json'
 import { collection, getDocs } from 'firebase/firestore/lite'
 import { db } from '../../firebase'
 
@@ -37,11 +36,11 @@ const Portfolio = () => {
                         return (
                             <div className="image-box" key={idx}>
                                 <img 
-                                src={port.cover}
+                                src={port.image}
                                 className="portfolio-image"
                                 alt="portfolio" />
                                 <div className="content">
-                                    <p className="title">{port.title}</p>
+                                    <p className="title">{port.name}</p>
                                     <h4 className="description">{port.description}</h4>
                                     <button
                                         className="btn"
@@ -68,7 +67,7 @@ const Portfolio = () => {
                         idx={15}/>
                 </h1>
 
-                <div>{renderPortfolio(portfolioData.portfolio)}</div>
+                <div>{renderPortfolio(portfolio)}</div>
                 
             </div>
 
