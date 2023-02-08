d1f9f54ee677fd261d334324739a203dcdfe4a54
Victoria Le <leqvictoria@gmail.com>
2023-02-08 16:51:29 -0500

    resume attached

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,8 +3,9 @@ import './index.scss'
 import LogoV from '../../assets/images/logo-v.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faSuitcase, faUser, faBars, faClose } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faSuitcase, faUser, faBars, faClose, faFile} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import  VLResume from '../../assets/Victoria Le - Resume - GA.pdf'
 import { useState } from 'react'
 
 const Sidebar = () => {
@@ -52,6 +53,14 @@ const Sidebar = () => {
                         <FontAwesomeIcon icon={faGithub} />
                     </a>
                 </li>
+
+                <li>
+                    <a target='_blank' rel='noopener noreferrer' href={VLResume}>
+                        <FontAwesomeIcon icon={faFile} />
+                    </a>
+                </li>
+
+                
             </ul>
             {/* END OF OUTSIDE LINKS */}
 
